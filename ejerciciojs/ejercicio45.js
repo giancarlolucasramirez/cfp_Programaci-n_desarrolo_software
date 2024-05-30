84586fda981a2dc8b04a80c6555ad52e547391f0
Giancarlo Joseph Andrew Lucas Ramírez <giancarlolucas@CFPBARTOLOMEAMBROSIO.COM>
2024-05-30 13:37:23 -0600

    se guardó cambio del ejercicio 44

--- ejerciciojs/ejercicio45.js
+++ ejerciciojs/ejercicio45.js
@@ -1,15 +1,12 @@
 let caracter = 'a';
 let encontrado = false;
-
 let array = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-
 array.forEach(function(elemento) {
     if (elemento === caracter) {
-        console.log('El carácter', caracter, 'se encuentra en el array');
+        console.log('El carácter', caracter, 'se encuentra e n el array');
         encontrado = true;
     }
 });
-
 if (!encontrado) {
     console.log('El carácter', caracter, 'no se encuentra en el array');
 }
