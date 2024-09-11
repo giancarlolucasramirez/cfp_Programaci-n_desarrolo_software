bb79e91e56acb9d8fa3c2689e6eb4c337a5daaad
Giancarlo Joseph Andrew Lucas Ramírez <giancarlolucas@CFPBARTOLOMEAMBROSIO.COM>
2024-09-11 15:11:43 -0600

    modulo1 pseudocódigo y html y css

--- modulo1/htmlycss/proyectoHtmlCssJs/p6/palabras.js
+++
@@ -1,1 +0,0 @@
-const words = ['Carne','Martillo', 'Lavadora','Sucio','Cangrejo','Lento','Alimentos','Delgado','Cubo','Comida','Caracol','Abajo','Alumno','Bonito','Cesta','Sol','Beber','Botella','Hamburguesa','Invierno'];
