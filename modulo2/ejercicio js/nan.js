cb16177e4723b818fbda6169ed429944e396b79b
Giancarlo Joseph Andrew Lucas Ramírez <giancarlolucas@CFPBARTOLOMEAMBROSIO.COM>
2024-09-11 11:35:14 -0600

    tetris

--- modulo2/ejercicio js/nan.js
+++ modulo2/ejercicio js/nan.js
@@ -6,18 +6,19 @@ emogi
 la pieza aparecera en la parte superior  izquierda de la pantalla desarollar una funcion capaz desplazar y rotar 
 la pieza en el tablero, recibiendo una accion cada vez que se llame, desplazar y rotar la pieza
 */
-// let currentPiece = generatePiece(); 
- // Define the size of the Tetris board
 
-let tetris = new Array(10)
 
-console.log(tetris.fill("⬜"));
+function tetrix() {
 
-for (let i = 0; i <= 10; i++) { 
-  console.log(tetris)
+  let tetris = new Array(10)
+
+  console.log(tetris.fill("⬜"));
+
+  for (let i = 0; i <= 10; i++) {
+    console.log(tetris)
+  }
+
 }
-
-
 
 
 function generar_pieza() {
@@ -25,17 +26,19 @@ function generar_pieza() {
   let type = Math.floor(Math.random() * 7);
   switch (type) {
     case 0: // I-piece
-    pieza = [[1, 1, 1, 1]];
+      pieza = [[1, 1, 1, 1]];
       break;
     case 1: // J-piece
-    pieza = [[1, 0, 0], [1, 1, 1]];
+      pieza = [[1, 0, 0], [1, 1, 1]];
       break;
     case 2: // L-piece
-    pieza = [[0, 0, 1], [1, 1, 1]];
+      pieza = [[0, 0, 1], [1, 1, 1]];
       break;
     case 3: // O-piece
-    pieza = [[1, 1], [1, 1]];
+      pieza = [[1, 1], [1, 1]];
       break;
   }
   return pieza;
 }
+
+//  tetrix()
