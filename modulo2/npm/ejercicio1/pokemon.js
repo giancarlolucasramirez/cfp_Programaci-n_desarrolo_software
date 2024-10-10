a976dcf8f825ec3cded4c1220cb2dd47e0d9ee75
giancarlolucasramirez <josephandrew1906@gmail.com>
2024-10-10 15:47:49 -0600

    ejercicio pokedex

--- modulo2/npm/ejercicio1/pokemon.js
+++ modulo2/npm/ejercicio1/pokemon.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const products = [
-  { id: 1, image: "", name: 'Bulbasaur', power: "Grass · Poison" },
-  { id: 2, image: "", name: 'Ivysaur', power: "Grass · Poison" },
-  { id: 3, image: "", name: 'Venusaur', power: "Grass · Poison" },
-  { id: 4, image: "", name: 'Charmander', power: "Fire" }
+  {  image: "",id: 1,  name: 'Bulbasaur', power: "Grass · Poison" },
+  {  image: "",id: 2,  name: 'Ivysaur', power: "Grass · Poison" },
+  {  image: "",id: 3,  name: 'Venusaur', power: "Grass · Poison" },
+  {  image: "",id: 4,  name: 'Charmander', power: "Fire" }
 ];
 
 app.get('/', (req, res) => {
-  res.json(products);
+  res.json(products[1]); // Devuelve solo el primer Pokémon
 });
 
 // Nueva ruta para obtener un Pokémon por ID
 app.get('/pokemon/:id', (req, res) => {
+  // Se utiliza req.params.id para obtener el parámetro de la URL.
+
   const id = parseInt(req.params.id);
   const product = products.find(p => p.id === id);
-  
+
+  /* Busca en el array products el Pokémon correspondiente. Si lo encuentra, lo devuelve como respuesta; 
+  si no, devuelve un error 404 con un mensaje indicando que no se encontró.*/
+
   if (product) {
     res.json(product);
   } else {
     res.status(404).send('Pokémon no encontrado');
   }
 });
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
