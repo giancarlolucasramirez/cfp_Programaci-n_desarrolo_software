6885097323ecde643caa23766189b2456c3dc75d
giancarlolucasramirez <josephandrew1906@gmail.com>
2024-10-31 15:38:00 -0600

    prueba 1

--- modulo3/prueba1/ejercicio3.js
+++ modulo3/prueba1/ejercicio3.js
@@ -1,10 +1,8 @@
 let numeros = [0,0,4,4]
-let resultado = numeros.some((numero, indice) => (numero === 0 && numeros[indice + 1] === 0) ) || numeros.some((numero, indice) => (numero === 4 && numeros[indice + 1] === 4) )
+let resultado = numeros.some((numero, indice) => (numero === 0 && numeros[indice + 1] === 0)) && numeros.some((numero, indice) => (numero === 4 && numeros[indice + 1] === 4))
 
 if( resultado){
-    console.log(true);
-} else if (numeros.includes(4) && numeros.includes(0)) {
     console.log(false);
+} else if (numeros.some((numero, indice) => (numero === 0 && numeros[indice + 1] === 0)) || numeros.some((numero, indice) => (numero === 4 && numeros[indice + 1] === 4))) {
+    console.log(true);
 }
-
-
