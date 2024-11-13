14b5365b04b9de579d110eb8aa4264fb00893ad2
giancarlolucasramirez <josephandrew1906@gmail.com>
2024-11-13 11:29:15 -0600

    ejercicio desestructuracion

--- modulo3/desestructuracion/ejercicio5.js
+++ modulo3/desestructuracion/ejercicio5.js
@@ -1,9 +1,10 @@
-let points = [x=1,y=1,x=2,y=2,1,2,3,4]
+let points = [[x=1,y=1],[x=2,y=2]]
 
-let par1 = points[0,1]
-let par2 = points[2,3]
+let par1 = points[0]
+let par2 = points[1]
 
-console.log(points)
 console.log(par1);
 console.log(par2);
+
+
 
